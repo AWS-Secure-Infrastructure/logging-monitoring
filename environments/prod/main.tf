e99af261baa8b35aeacdad935befa8ba12ec0195
SebaSilC <sebastian@playbookvisualarts.com>
2026-02-15 14:24:27 +0100

    Create main.tf

---
+++ environments/prod/main.tf
@@ -0,0 +1,63 @@
+module "kms" {
+  source = "../../modules/kms"
+
+  name = "${var.project_name}-kms"
+
+  tags = var.tags
+}
+
+module "logging_bucket" {
+  source = "../../modules/logging_bucket"
+
+  bucket_name = "${var.project_name}-logs-${data.aws_caller_identity.current.account_id}"
+  kms_key_arn = module.kms.key_arn
+
+  tags = var.tags
+}
+
+data "aws_caller_identity" "current" {}
+
+module "cloudtrail" {
+  source = "../../modules/cloudtrail"
+
+  name           = "${var.project_name}-trail"
+  s3_bucket_name = module.logging_bucket.bucket_id
+  kms_key_arn    = module.kms.key_arn
+
+  tags = var.tags
+}
+
+module "config" {
+  source = "../../modules/config"
+
+  name           = var.project_name
+  s3_bucket_name = module.logging_bucket.bucket_id
+
+  tags = var.tags
+}
+
+module "guardduty" {
+  source = "../../modules/guardduty"
+
+  tags = var.tags
+}
+
+module "cloudwatch_alerts" {
+  source = "../../modules/cloudwatch_alerts"
+
+  name           = var.project_name
+  log_group_name = "/aws/cloudtrail/${var.project_name}"
+  alarm_email    = var.alarm_email
+
+  tags = var.tags
+}
+
+module "athena" {
+  source = "../../modules/athena"
+
+  name                  = var.project_name
+  s3_bucket_name        = module.logging_bucket.bucket_id
+  athena_results_bucket = module.logging_bucket.bucket_id
+
+  tags = var.tags
+}
